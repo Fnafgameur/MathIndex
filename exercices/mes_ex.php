5694e4e52276f45c451bc59ed8216432b4aaeb83
Theo Alleaume <theo.alleaume024@gmail.com>
2024-03-31 13:16:50 +0200

    Creating front of my exercices page(mes_ex.php) 	renamed:    assets/css/style.css -> assets/css/style.scss 	modified:   exercices/mes_ex.php 	modified:   includes/header.php 	modified:   includes/sidebar.php

---
+++ exercices/mes_ex.php
@@ -0,0 +1,34 @@
+<?php 
+include_once("../includes/header.php");
+include_once("../includes/sidebar.php");
+?>
+
+<div class="exercices">
+    <h1 class="exercices__title">Mes exercices</h1>
+    <div class="exercices__content">
+        <p class="exercices__description">Vous pouvez modifier ou supprimer un de vos exercices.</p>
+        <table class="exercices__table">
+            <thead class="exercices__table-head">
+                <tr class="exercices__table-row">
+                    <th class="exercices__table-heading">Nom</th>
+                    <th class="exercices__table-heading">Thématique</th>
+                    <th class="exercices__table-heading">Fichiers</th>
+                    <th class="exercices__table-heading">Actions</th>
+                </tr>
+            </thead>
+            <tbody class="exercices__table-body">
+                <tr class="exercices__table-row">
+                    <td class="exercices__table-data">test</td>
+                    <td class="exercices__table-data">test</td>
+                    <td class="exercices__table-data">test</td>
+                    <td class="exercices__table-data">test</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+    
+
+<?php include_once("../includes/footer.php");
+?>
