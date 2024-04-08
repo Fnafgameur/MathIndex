29a26c8ad457dd3602cac92a4fce80b061a34769
Fnafgameur <chara60etdashi@gmail.com>
2024-04-08 21:08:47 +0200

    Removed Visiteur role from enum

--- enums/Role.php
+++ enums/Role.php
@@ -4,7 +4,6 @@ enum Role: string
 {
     case ADMIN = "Administrateur";
     case CONTRIBUTOR = "Contributeur";
-    case GUEST = "Visiteur";
 
     public static function isGranted(string $role) : bool {
         return in_array($role, [Role::ADMIN->value, Role::CONTRIBUTOR->value]);
