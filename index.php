111dcffdce988d653a5a8381c6d1322d065e4db1
Milan Juino <milanjuino@gmail.com>
2024-04-08 08:43:47 +0200

    Created acceuil.php Added switch in index.php

--- index.php
+++ index.php
@@ -3,5 +3,26 @@ include_once("includes/header.php");
 
 include_once("includes/sidebar.php");
 
+if (isset($_GET["page"])){
+    switch ($_GET["page"]){
+        case "Mes+exercices":
+            include_once("exercices/mes_ex.php");
+            break;
+        case "Recherche":
+            include_once("exercices/recherche_ex.php");
+            break;
+        case "Mathématique":
+            include_once("exercices/mathematiques.php");
+            break;
+        case "Soumettre":
+            include_once("exercices/soumettre_ex.php");
+            break;
+        default:
+            include_once("exercices/accueil.php");
+            break;
+    }
+} else {
+    include_once("exercices/accueil.php");
+}
 
 include_once("includes/footer.php");
