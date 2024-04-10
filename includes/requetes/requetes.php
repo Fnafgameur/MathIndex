4591c5ed7cdb41d2644952ce3225a708177385a6
Theo Alleaume <theo.alleaume024@gmail.com>
2024-04-10 22:55:48 +0200

    Added back functionality for exercice page (download link)

--- includes/requetes/requetes.php
+++ includes/requetes/requetes.php
@@ -3,35 +3,26 @@
 include_once 'includes/db.php';
 
 if (isset($db)) {
-
-    /**
-     * Permet d'obtenir les infos d'un utilisateur stocké en DB avec son email
-     * @param string $email - L'email de l'utilisateur
-     * @return mixed - Retourne un tableau associatif contenant les informations de l'utilisateur ou false si l'utilisateur n'existe pas
-     */
-    function get_user_with_email(string $email): mixed
-    {
+    function get_user_with_email($email) : mixed {
         global $db;
-        $query = $db->prepare("SELECT * FROM user WHERE email = :email");
+        $query = $db->prepare("SELECT * FROM users WHERE email = :email");
         $query->bindParam(':email', $email);
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
-
-    /**
-     * Permet d'obtenir tous les exercices stockés en DB
-     * @return array|false - Retourne un tableau associatif contenant les informations de tous les exercices ou false si aucun exercice n'existe
-     */
-    function get_exercices() : array|false
-    {
+    function get_exercices() : mixed {
         global $db;
         $query = $db->prepare("SELECT * FROM exercise");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-}
 
-if (isset($db)) {
+    function get_exercices_sorted() : mixed {
+        global $db;
+        $query = $db->prepare("SELECT * FROM exercise ORDER BY date_uploaded ASC");
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
     function get_thematic_by_exercices($exercice_thematic) : mixed {
         global $db;
         $query = $db->prepare("SELECT thematic.name from thematic INNER JOIN exercise ON thematic.id = exercise.thematic_id WHERE thematic.id = :exercice;");
@@ -39,17 +30,18 @@ if (isset($db)) {
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
-}
-
-if (isset($db)) {
     function get_file_by_exercices($exercice_file) : mixed {
         global $db;
-        $query = $db->prepare("SELECT file.name,extension from file INNER JOIN exercise ON file.id = exercise.exercise_file_id WHERE file.id = :exercice;");
+        $query = $db->prepare("SELECT * from file INNER JOIN exercise ON file.id = exercise.exercise_file_id WHERE file.id = :exercice;");
         $query->bindParam(':exercice', $exercice_file);
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
 }
+
+
+
+
 
 
 
