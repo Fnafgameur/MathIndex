72e0e04f126992b4feefacb2aa69537ca00caa67
Theo Alleaume <theo.alleaume@lyceestvincent.fr>
2024-04-15 14:43:21 +0200

    Added style for keywords and limited exercise per page

--- exercices/mathematiques.php
+++ exercices/mathematiques.php
@@ -1,6 +1,7 @@
 <?php
-    $exercices = get_exercices();
-    $exercices_sorted = get_exercices_sorted();
+    $exercises = get_exercises();
+    $exercises_sorted = get_exercises_sorted();
+    //explode(',',$exercise_sorted['keywords'])
 ?>
 <div class="mathematics">
     <h1 class="mathematics__title">Exercices</h1>
@@ -12,26 +13,30 @@
             <tr class="mathematics__table-row">
                 <th class="mathematics__table-heading">Nom</th>
                 <th class="mathematics__table-heading">Thématiques</th>
-                <th class="mathematics__table-heading">Difficulté</th>
-                <th class="mathematics__table-heading">Durée</th>
-                <th class="mathematics__table-heading">Mots-clés</th>
+                <th class="mathematics__table-heading difficulty">Difficulté</th>
+                <th class="mathematics__table-heading duration">Durée</th>
+                <th class="mathematics__table-heading keywords">Mots-clés</th>
                 <th class="mathematics__table-heading">Fichiers</th>
             </tr>
             </thead>
             <tbody class="mathematics__table-body">
-            <?php foreach ($exercices_sorted as $exercice_sorted) {
-                $file = get_file_by_exercices($exercice_sorted['exercise_file_id']);
+            <?php foreach ($exercises_sorted as $exercise_sorted) {
+                $file_sorted = get_file_by_exercises($exercise_sorted['exercise_file_id']);
+                $result_array = explode(',', $exercise_sorted['keywords']);
                 ?>
                 <tr class="mathematics__table-row">
-                    <td class="mathematics__table-data"><?=$exercice_sorted['name']?></td>
-                    <td class="mathematics__table-data"><?=get_thematic_by_exercices($exercice_sorted["thematic_id"])['name'];?>
+                    <td class="mathematics__table-data"><?=$exercise_sorted['name']?></td>
+                    <td class="mathematics__table-data"><?=get_thematic_by_exercises($exercise_sorted["thematic_id"])['name'];?>
                     </td>
-                    <td class="mathematics__table-data"><?=$exercice_sorted['difficulty']?></td>
-                    <td class="mathematics__table-data"><?=$exercice_sorted['duration']??"NULL gros gay"?></td>
-                    <td class="mathematics__table-data"><?=$exercice_sorted['keywords']??"NULL gros gay"?></td>
+                    <td class="mathematics__table-data"><?=$exercise_sorted['difficulty']?></td>
+                    <td class="mathematics__table-data"><?=$exercise_sorted['duration']??"Aucun"?></td>
+                    <td class="mathematics__table-data wordbreak"><?php foreach ($result_array as $result) {
+                            echo "<p class='keyword'>$result</p>";
+                        } ?>
+                    </td>
                     <td class="mathematics__table-data">
-                        <a class="link link--row" href="" download="<?=$file['name'].'.'.$file['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Exercice</a>
-                        <a class="link link--row" href="" download="<?=$file['name'].'_corrigé.'.$file['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Corrigé</a>
+                        <a class="link link--row" href="" download="<?=$file_sorted['name'].'.'.$file_sorted['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Exercice</a>
+                        <a class="link link--row" href="" download="<?=str_contains(strtolower($file_sorted['name']),'_corrigé')?$file_sorted['name'].'_corrigé':$file_sorted['name'].$file_sorted['extension'];?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Corrigé</a>
                     </td>
                 </tr>
             <?php } ?>
@@ -45,26 +50,29 @@
                 <tr class="mathematics__table-row">
                     <th class="mathematics__table-heading">Nom</th>
                     <th class="mathematics__table-heading">Thématiques</th>
-                    <th class="mathematics__table-heading">Difficulté</th>
-                    <th class="mathematics__table-heading">Durée</th>
-                    <th class="mathematics__table-heading">Mots-clés</th>
+                    <th class="mathematics__table-heading difficulty">Difficulté</th>
+                    <th class="mathematics__table-heading duration">Durée</th>
+                    <th class="mathematics__table-heading keywords">Mots-clés</th>
                     <th class="mathematics__table-heading">Fichiers</th>
                 </tr>
                 </thead>
                 <tbody class="mathematics__table-body">
-                <?php foreach ($exercices as $exercice) {
-                    $file = get_file_by_exercices($exercice['exercise_file_id']);
+                <?php foreach ($exercises as $exercise) {
+                    $file = get_file_by_exercises($exercise['exercise_file_id']);
                     ?>
                     <tr class="mathematics__table-row">
-                        <td class="mathematics__table-data"><?=$exercice['name']?></td>
-                        <td class="mathematics__table-data"><?=get_thematic_by_exercices($exercice["thematic_id"])['name'];?>
+                        <td class="mathematics__table-data"><?=$exercise['name']?></td>
+                        <td class="mathematics__table-data"><?=get_thematic_by_exercises($exercise["thematic_id"])['name'];?>
                         </td>
-                        <td class="mathematics__table-data"><?=$exercice['difficulty']?></td>
-                        <td class="mathematics__table-data"><?=$exercice['duration']??"NULL gros gay"?></td>
-                        <td class="mathematics__table-data"><?=$exercice['keywords']??"NULL gros gay"?></td>
+                        <td class="mathematics__table-data"><?=$exercise['difficulty']?></td>
+                        <td class="mathematics__table-data"><?=$exercise['duration']??"Aucun"?></td>
+                        <td class="mathematics__table-data wordbreak"><?php foreach ($result_array as $result) {
+                                echo "<p class='keyword'>$result</p>";
+                            } ?>
+                        </td>
                         <td class="mathematics__table-data">
-                            <a class="link link--row" href="" download="<?=$file['name'].'.'.$file['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Exercice</a>
-                            <a class="link link--row" href="" download="<?=$file['name'].'_corrigé.'.$file['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Corrigé</a>
+                            <a class="link link--row" href="" download="<?=$file_sorted['name'].'.'.$file_sorted['extension']?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Exercice</a>
+                            <a class="link link--row" href="" download="<?=str_contains(strtolower($file_sorted['name']),'_corrigé')?$file_sorted['name'].'_corrigé':$file_sorted['name'].$file_sorted['extension'];?>"><img src="./assets/icons/download_file.svg" alt="logo téléchargement">Corrigé</a>
                         </td>
                     </tr>
                 <?php } ?>
--- includes/requetes/requetes.php
+++ includes/requetes/requetes.php
@@ -5,35 +5,35 @@ include_once 'includes/db.php';
 if (isset($db)) {
     function get_user_with_email($email) : mixed {
         global $db;
-        $query = $db->prepare("SELECT * FROM users WHERE email = :email");
+        $query = $db->prepare("SELECT * FROM user WHERE email = :email");
         $query->bindParam(':email', $email);
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
-    function get_exercices() : mixed {
+    function get_exercises() : mixed {
         global $db;
-        $query = $db->prepare("SELECT * FROM exercise");
+        $query = $db->prepare("SELECT * FROM exercise LIMIT 5");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function get_exercices_sorted() : mixed {
+    function get_exercises_sorted() : mixed {
         global $db;
-        $query = $db->prepare("SELECT * FROM exercise ORDER BY date_uploaded ASC");
+        $query = $db->prepare("SELECT * FROM exercise ORDER BY date_uploaded ASC LIMIT 3");
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-    function get_thematic_by_exercices($exercice_thematic) : mixed {
+    function get_thematic_by_exercises($exercise_thematic) : mixed {
         global $db;
-        $query = $db->prepare("SELECT thematic.name from thematic INNER JOIN exercise ON thematic.id = exercise.thematic_id WHERE thematic.id = :exercice;");
-        $query->bindParam(':exercice', $exercice_thematic);
+        $query = $db->prepare("SELECT thematic.name from thematic INNER JOIN exercise ON thematic.id = exercise.thematic_id WHERE thematic.id = :exercise;");
+        $query->bindParam(':exercise', $exercise_thematic);
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
-    function get_file_by_exercices($exercice_file) : mixed {
+    function get_file_by_exercises($exercise_file) : mixed {
         global $db;
-        $query = $db->prepare("SELECT * from file INNER JOIN exercise ON file.id = exercise.exercise_file_id WHERE file.id = :exercice;");
-        $query->bindParam(':exercice', $exercice_file);
+        $query = $db->prepare("SELECT file.id, file.name, file.extension, file.size from file INNER JOIN exercise ON file.id = exercise.exercise_file_id WHERE file.id = :exercise;");
+        $query->bindParam(':exercise', $exercise_file);
         $query->execute();
         return $query->fetch(PDO::FETCH_ASSOC);
     }
