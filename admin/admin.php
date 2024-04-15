2781459475eb22260231e5945e8b6a371b88aa3c
Milan Juino <milanjuino@gmail.com>
2024-04-15 11:56:29 +0200

    fixed admin_contributors_page css

--- admin/admin.php
+++ admin/admin.php
@@ -2,32 +2,23 @@
 <div class="administration">
     <h1 class="administration__title">Administration</h1>
     <div class="administration__content">
-        <ul>
-            <li>
-                <button type="button" class="administration__buttons-button">Contributeurs</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Exercices</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Matières</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Classes</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Thématiques</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Niveaux</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Compétences</button>
-            </li>
-            <li>
-                <button type="button" class="administration__buttons-button">Origines</button>
-            </li>
-        </ul>
+        <div class="administration__head-bar">
+            <button type="button" class="administration__buttons-button">Contributeurs</button>
+        
+            <button type="button" class="administration__buttons-button">Exercices</button>
+        
+            <button type="button" class="administration__buttons-button">Matières</button>
+        
+            <button type="button" class="administration__buttons-button">Classes</button>
+        
+            <button type="button" class="administration__buttons-button">Thématiques</button>
+        
+            <button type="button" class="administration__buttons-button">Niveaux</button>
+        
+            <button type="button" class="administration__buttons-button">Compétences</button>
+        
+            <button type="button" class="administration__buttons-button">Origines</button>
+        </div>
         <div class="administration__gestion">
             <?php include_once("admin/gestion_pages/contributeurs.php"); ?>
         </div>
--- admin/gestion_pages/contributeurs.php
+++ admin/gestion_pages/contributeurs.php
@@ -1,19 +1,24 @@
 
-<div class="contributeurs">
-    <h3 class="contributeurs__title">Contributeurs</h3>
-    <p class="contributeurs__description">Rechercher un contributeur par nom, prénom ou email :</p>
-    <form action="#" method="GET" class="contributeurs__form">
-        <input type="text" name="search" id="search" class="contributeurs__input" placeholder="Rechercher">
-        <button type="submit" class="contributeurs__button">Ajouter +</button>
-    </form>
-    <table class="contributeurs__table">
-        <thead>
+<div class="contributors">
+    <h2 class="contributors__title">Gestion des contributeurs</h2>
+    <p class="contributors__description">Rechercher un contributeur par nom, prénom ou email :</p>
+    <div class="contributors__action-bar">
+        <form action="#" method="GET" class="contributeurs__form">
+            <input type="text" name="search" id="search" class="contributeurs__input" placeholder="Rechercher">
+            <button type="submit">Rechercher</button>
+        </form>
+        <a>
+            <button type="submit">Ajouter +</button>
+        </a>
+    </div>
+    <table class="contributors__table">
+        <thead class="recherche__table-head">
             <tr>
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Rôle</th>
                 <th>Email</th>
-                <th>Action</th>
+                <th class="action">Action</th>
             </tr>
         </thead>
         <tbody>
@@ -23,8 +28,8 @@
                 <td>Enseignant</td>
                 <td>mail@mail.com</td>
                 <td>
-                    <button type="button" class="contributeurs__button">Modifier</button>
-                    <button type="button" class="contributeurs__button">Supprimer</button>
+                    <button type="button" class="contributeurs__button"><img src="assets/icons/edit_file.svg">Modifier</button>
+                    <button type="button" class="contributeurs__button"><img src="assets/icons/delete_file.svg">Supprimer</button>
                 </td>
             </tr>
         </tbody>
