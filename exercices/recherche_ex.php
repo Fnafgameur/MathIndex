f8104afc4495d12ee385240400e13a568c181430
Djimmy Cotte <djimmy.c.60660@gmail.com>
2024-04-15 13:27:40 +0200

    Added filter system for search exercise Changed get_exercices() function
    TODO : check if user didn't changed values with inspect tab

--- exercices/recherche_ex.php
+++ exercices/recherche_ex.php
@@ -1,26 +1,55 @@
+<?php
+
+    $exercices = [];
+
+    $filtres = [
+            "niveau" => "",
+            "thematique" => "",
+            "mots-cles" => "",
+    ];
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST)) {
+        $filtres["niveau"] = $_POST["niveau"];
+        $filtres["thematique"] = $_POST["thematique"];
+        $filtres["mots-cles"] = $_POST["mots-cles"];
+        $exercices = get_exercices($filtres);
+
+    } else {
+        $exercices = get_exercices();
+    }
+
+    $number = $exercices["number"]??0;
+    $exercices = $exercices["exercise"];
+?>
+
 <div class="recherche">
     <h1 class="recherche__title">Rechercher un exercice</h1>
     <div class="recherche__content">
-        <form class="recherche__form">
+        <form class="recherche__form" action="#" method="post">
             <div>
                 <label for="niveau">Niveau:</label>
                 <select name="niveau">
-                    <option id="niveau" name="niveau">Collège</option>
-                    <option id="niveau"  name="niveau">Lycée</option>
-                    <option id="niveau"  name="niveau">Supérieur</option>
+                    <option id="niveau" name="niveau" value="1" <?= $filtres["niveau"] === "1" ? "selected" : "" ?>>Seconde</option>
+                    <option id="niveau" name="niveau" value="2" <?= $filtres["niveau"] === "2" ? "selected" : "" ?>>Première</option>
+                    <option id="niveau" name="niveau" value="3" <?= $filtres["niveau"] === "3" ? "selected" : "" ?>>Terminale</option>
                 </select>
             </div>
             <div>
                 <label for="thematique">Thématique:</label>
                 <select name="thematique">
-                    <option id="thematique"  name="thematique">Suites</option>
-                    <option id="thematique"  name="thematique">Algèbre de bol</option>
-                    <option id="thematique"  name="thematique">Pytagore</option>
+                    <option id="thematique"  name="thematique" value="1" <?= $filtres["thematique"] === "1" ? "selected" : "" ?>>Suites</option>
+                    <option id="thematique"  name="thematique" value="2" <?= $filtres["thematique"] === "2" ? "selected" : "" ?>>Primitives</option>
+                    <option id="thematique"  name="thematique" value="3" <?= $filtres["thematique"] === "3" ? "selected" : "" ?>>Algèbres</option>
+                    <option id="thematique"  name="thematique" value="4" <?= $filtres["thematique"] === "4" ? "selected" : "" ?>>Continuités</option>
+                    <option id="thematique"  name="thematique" value="5" <?= $filtres["thematique"] === "5" ? "selected" : "" ?>>Matriciel</option>
+                    <option id="thematique"  name="thematique" value="6" <?= $filtres["thematique"] === "6" ? "selected" : "" ?>>Géométrie</option>
+                    <option id="thematique"  name="thematique" value="7" <?= $filtres["thematique"] === "7" ? "selected" : "" ?>>Trigonométrie</option>
+                    <option id="thematique"  name="thematique" value="8" <?= $filtres["thematique"] === "8" ? "selected" : "" ?>>Fonctions et équations</option>
                 </select>
             </div>
             <div>
                 <label for="mots-cles">Mots clés:</label>
-                <input id="mots_cles" name="mots-cles">
+                <input id="mots_cles" name="mots-cles" <?= $filtres["mots-cles"] === "" ? "" : "value=".$filtres["mots-cles"] ?>>
             </div>
             <div>
                 <button type="submit">
@@ -29,7 +58,7 @@
             </div>
         </form>
         
-        <h2>n° exrcices trouvés</h2>
+        <h2><?= $number; ?> exercices trouvés</h2>
 
         <table class="recherche__table">
             <thead class="recherche__table-head">
@@ -42,13 +71,19 @@
                 </tr>
             </thead>
             <tbody class="recherche__table-body">
+            <?php
+                foreach ($exercices as $exercice) { ?>
+
                 <tr class="recherche__table-row">
-                    <td class="recherche__table-data">test</td>
-                    <td class="recherche__table-data">test</td>
-                    <td class="recherche__table-data">test</td>
-                    <td class="recherche__table-data">test</td>
-                    <td class="recherche__table-data">test</td>
+                    <td class="recherche__table-data"><?= $exercice["name"] ?></td>
+                    <td class="recherche__table-data"><?= $exercice["difficulty"] ?></td>
+                    <td class="recherche__table-data"><?= $exercice["keywords"] ?></td>
+                    <td class="recherche__table-data"><?= $exercice["duration"] ?></td>
+                    <td class="recherche__table-data">
+                        <a href="<?= $exercice["file"]??"" ?>" download="<?= $exercice["file"]??"" ?>">Télécharger</a>
+                    </td>
                 </tr>
+            <?php } ?>
             </tbody>
         </table>
     </div>
--- includes/requetes/requetes.php
+++ includes/requetes/requetes.php
@@ -22,12 +22,50 @@ if (isset($db)) {
      * Permet d'obtenir tous les exercices stockés en DB
      * @return array|false - Retourne un tableau associatif contenant les informations de tous les exercices ou false si aucun exercice n'existe
      */
-    function get_exercices() : array|false
+    function get_exercices(array $filtres = null) : array|false
     {
         global $db;
-        $query = $db->prepare("SELECT * FROM exercise");
+
+        if (isset($filtres)) {
+            $result = [
+                "exercise" => [],
+                "number" => 0,
+            ];
+
+            $niveau = $filtres["niveau"];
+            $thematique = $filtres["thematique"];
+            $mots_cles = explode(" ", $filtres["mots-cles"]);
+
+            $keywordsReq = "";
+
+            foreach ($mots_cles as $mot) {
+                $keywordsReq .= "keywords LIKE '%$mot%'";
+                if ($mot !== end($mots_cles)) {
+                    $keywordsReq .= " OR ";
+                }
+            }
+
+            if ($keywordsReq === "") {
+                $query = $db->prepare("SELECT * FROM exercise WHERE classroom_id = :niveau AND thematic_id = :thematique");
+            }
+            else {
+                $query = $db->prepare("SELECT * FROM exercise
+            WHERE classroom_id = :niveau
+            AND thematic_id = :thematique
+            AND $keywordsReq");
+            }
+            $query->bindParam(':niveau', $niveau);
+            $query->bindParam(':thematique', $thematique);
+        } else {
+            $query = $db->prepare("SELECT * FROM exercise");
+        }
+
         $query->execute();
-        return $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $result["exercise"] = $query->fetchAll(PDO::FETCH_ASSOC);
+        $result["number"] = $query->rowCount();
+
+        return $result;
     }
 }
 
