a217714f1f9ae7dba685828786af7ec93240d30b
Milan Juino <milanjuino@gmail.com>
2024-04-29 14:15:05 +0200

    Added feature upload file on the submit page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,3 @@
-// Javascript of the modal
-const modalContainer = document.querySelector(".modal__container");
-const modalTriggers = document.querySelectorAll(".modal__trigger");
-
-modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
-
-function toggleModal(){
-    modalContainer.classList.toggle("modal__container--active");
-}
-function sendData(element){
-    document.querySelector('.modal_delete_form input[name="id"]').value = element.querySelector('input[name="id"]').value;
-}
-
 var information = document.getElementById("submit__information");
 var source = document.getElementById("submit__source");
 var file = document.getElementById("submit__file");
@@ -20,6 +7,11 @@ var btn_s = document.getElementById("btn_source");
 var btn_f = document.getElementById("btn_file");
 var next_s = document.getElementById("next_source");
 var next_f = document.getElementById("next_file");
+
+var btn_exo = document.getElementById("fichier_exercice");
+var btn_cor = document.getElementById("ficher_correction");
+var txt_exo = document.getElementById("fichier_exercice_choisit");
+var txt_cor = document.getElementById("fichier_correction_choisit");
 
 btn_i.addEventListener("click",function(){
     if (information.style.display === "none"){
@@ -86,3 +78,24 @@ next_f.addEventListener("click",function(){
     }
 });
 
+
+btn_exo.addEventListener('change', function(){
+    txt_exo.textContent = btn_exo.files[0].name;
+});
+
+btn_cor.addEventListener('change', function(){
+    txt_cor.textContent = btn_cor.files[0].name;
+});
+
+// Javascript of the modal
+const modalContainer = document.querySelector(".modal__container");
+const modalTriggers = document.querySelectorAll(".modal__trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+
+function toggleModal(){
+    modalContainer.classList.toggle("modal__container--active");
+}
+function sendData(element){
+    document.querySelector('.modal_delete_form input[name="id"]').value = element.querySelector('input[name="id"]').value;
+}
--- exercices/soumettre_ex.php
+++ exercices/soumettre_ex.php
@@ -1,3 +1,32 @@
+<?php
+    $uploads_exo = './assets/files/exercises/';
+    $uploads_cor = './assets/files/corrections/';
+    $alter = 0;
+    foreach ($_FILES as $file) {
+        if ($file["error"] === UPLOAD_ERR_OK) {
+            $tmp_name = $file["tmp_name"];
+            $name = basename($file["name"]);
+            if (($alter%2) === 0){
+                $uploads_dir = $uploads_exo ;
+                echo "exo";
+            } else {
+                $uploads_dir = $uploads_cor;
+                echo "cor";
+            }
+            $destination = $uploads_dir . $name ;
+            var_dump($destination);
+            if (move_uploaded_file($tmp_name, $destination )) {
+                echo "File uploaded successfully.";
+                $fileContents = file_get_contents($destination);
+            } else {
+                echo "Failed to move the file.";
+            }
+        }
+        $alter += 1;
+    }
+    
+?>
+
 <div class="submit">
     <h1 class="submit__title">Soumettre un exercice</h1>
     <div class="submit__head-bar">
@@ -8,37 +37,37 @@
         <button type="button" class="" id="btn_file">Fichiers</button>
     </div>
     <div class="submit__content">
-        <form action="#" method="post">
+        <form action="#" method="post" enctype="multipart/form-data">
             <div class="submit__information" id="submit__information">
                 <h2>Information générales</h2>
-                <div class="submit__information__flex" id="submit__information">
+                <div class="submit__information__flex">
 
                     <div>
                         <label for="nom">Nom de l'exercice: *</label><br>
                         <input id="nom" name="nom"><br>
                         <label for="classe">Classe: *</label><br>
-                        <select name="classe">
-                            <option id="classe" name="classe">Collège</option>
-                            <option id="classe"  name="classe">Lycée</option>
-                            <option id="classe"  name="classe">Supérieur</option>
+                        <select name="classe" id="classe">
+                            <option value="classe" style="padding:12rem">Collège</option>
+                            <option value="classe">Lycée</option>
+                            <option value="classe">Supérieur</option>
                         </select><br>
                         <label for="thematique">Thématique: *</label><br>
-                        <select name="thematique">
-                            <option id="thematique" name="thematique">Collège</option>
-                            <option id="thematique"  name="thematique">Lycée</option>
-                            <option id="thematique"  name="thematique">Supérieur</option>
+                        <select name="thematique"id="thematique">
+                            <option value="thematique">Listes</option>
+                            <option value="thematique">Pythagore</option>
+                            <option value="thematique">Thales</option>
                         </select><br>
                         <label for="chapitre">Chapitre du cours:</label><br>
                         <input id="chapitre"  name="chapitre">
                     </div>
                     <div>
                         <label for="mots_cles">Mots clés</label><br>
-                        <input id="chapitre"  name="chapitre"><br>
+                        <input id="mots_cles"  name="mots_cles"><br>
                         <label for="difficulte">Difficulté: *</label><br>
-                        <select name="difficulte">
-                            <option id="difficulte" name="difficulte">Collège</option>
-                            <option id="difficulte"  name="difficulte">Nigger</option>
-                            <option id="difficulte"  name="difficulte">Supérieur</option>
+                        <select name="difficulte" id="difficulte">
+                            <option value="difficulte">eazy</option>
+                            <option value="difficulte">hard</option>
+                            <option value="difficulte">ultra hard</option>
                         </select><br>
                         <label for="duree">Durée (en heure):</label><br>
                         <input id="duree"  name="duree">
@@ -47,13 +76,13 @@
                 <button type="button" id="next_source">Continuer</button>
             </div>
 
-            <div class="submit__source" id="submit__source">
+            <div class="submit__source" id="submit__source"  style="display: none;">
                 <h2>Sources</h2>
                 <label for="origine">Origine: *</label>
-                <select name="origine">
-                    <option id="origine" name="origine">Collège</option>
-                    <option id="origine"  name="origine">Nigger</option>
-                    <option id="origine"  name="origine">Supérieur</option>
+                <select name="origine" id="origine">
+                    <option value="origine">Collège</option>
+                    <option  value="origine">Nigger</option>
+                    <option  value="origine">Supérieur</option>
                 </select><br>
                 <label for="site">Nom de la source/lien du site: *</label>
                 <input id="site"  name="site">
@@ -62,21 +91,21 @@
                 <button type="button" id="next_file">Continuer</button>
             </div>
 
-            <div class="submit__file" id="submit__file">
+            <div class="submit__file" id="submit__file" style="display: none;">
                 <h2>Fichiers</h2>
                 <label for="fichier_exercice">fichier exercice: (pdf, docx)*<br>
                     <div>
-                        <p>Selectionné un fichier à télécharger</p>
+                        <p id="fichier_exercice_choisit">Selectionné un fichier à télécharger</p>
                         <img src="./assets/icons/upload_cloud.svg">
                     </div>
-                    <input type="file" accept="document/docx, document/pdf, document/txt" id="fichier_exercice"  name="fichier_exercice">
+                    <input type="file" accept=".docx, .pdf, .txt" id="fichier_exercice"  name="fichier_exercice" hidden/>
                 </label>
                 <label for="ficher_correction">fichier correction: (pdf, docx)*<br>
                     <div>
-                        <p>Selectionné un fichier à télécharger</p>
+                        <p id="fichier_correction_choisit">Selectionné un fichier à télécharger</p>
                         <img src="./assets/icons/upload_cloud.svg">
                     </div>
-                    <input type="file" accept="document/docx, document/pdf, document/txt" id="ficher_correction"  name="ficher_correction">
+                    <input type="file" accept=".docx, .pdf, .txt" id="ficher_correction"  name="ficher_correction" hidden/>
                 </label>
                 <button type="submit">Enregistrer</button>
             </div>
